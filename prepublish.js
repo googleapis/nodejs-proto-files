9e92cb19c1c4f582e8a219490430ee2edbe1ae64
Stephen Sawchuk <sawchuk@gmail.com>
2016-04-14 21:37:05 -0400

    re-write prepublish script to always pull from remotes

--- prepublish.js
+++ prepublish.js
@@ -5,24 +5,42 @@ var fs = require('fs')
 var got = require('got')
 var path = require('path')
 
-var input = 'https://github.com/google/googleapis/archive/master.zip'
-var output = './googleapis.zip'
-
 var throwErr = function (err) { if (err) throw err }
 
-got.stream(input)
-  .on('error', throwErr)
-  .pipe(fs.createWriteStream('./googleapis.zip'))
-  .on('error', throwErr)
-  .on('finish', function () {
-    var unzipper = new DecompressZip(output)
+var extract = function (input, opts) {
+  var output = Math.random() + '.zip'
 
-    unzipper
-      .on('error', throwErr)
-      .extract({
-        strip: 1,
-        filter: function (file) {
-          return path.extname(file.filename) === '.proto'
-        }
-      })
-  })
+  got.stream(input)
+    .on('error', throwErr)
+    .pipe(fs.createWriteStream(output))
+    .on('error', throwErr)
+    .on('finish', function () {
+      var unzipper = new DecompressZip(output)
+
+      unzipper
+        .on('error', throwErr)
+        .extract({
+          strip: opts.strip,
+          filter: function (file) {
+            if (opts.filter && !opts.filter(file)) return
+            return path.extname(file.filename) === '.proto'
+          }
+        })
+        .on('extract', function () {
+          fs.unlink(output, throwErr)
+        })
+    })
+}
+
+extract('https://github.com/google/googleapis/archive/master.zip', { strip: 1 })
+extract('https://github.com/google/protobuf/archive/master.zip', {
+  strip: 2,
+  filter: function (file) {
+    return file.parent.indexOf('protobuf-master') === 0 &&
+           file.parent.indexOf('protobuf-master/src/') === 0 &&
+           file.parent.indexOf('/compiler') === -1 &&
+           file.parent.indexOf('/internal') === -1 &&
+           file.filename.indexOf('unittest') === -1 &&
+           file.filename.indexOf('test') === -1
+  }
+})
