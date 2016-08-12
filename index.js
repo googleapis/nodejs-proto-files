2c174bc4f1a11019fe559cd603d275ed6dac71dd
Jason Dobry <jason.dobry@gmail.com>
2016-08-12 13:02:38 -0700

    Update for new proto files.

--- index.js
+++ index.js
@@ -7,6 +7,10 @@ var googleProtoFiles = module.exports = function () {
   return path.join.apply(null, [__dirname, 'google'].concat(args))
 }
 
+module.exports.appengine = {
+  v1: googleProtoFiles('appengine/v1/appengine.proto')
+}
+
 module.exports.bigtable = {
   admin: googleProtoFiles('bigtable/admin/table/v1/bigtable_table_service.proto'),
   v1: googleProtoFiles('bigtable/v1/bigtable_service.proto')
@@ -36,6 +40,14 @@ module.exports.logging = {
   v2: googleProtoFiles('logging/v2/logging.proto')
 }
 
+module.exports.monitoring = {
+  v3: {
+    agent: googleProtoFiles('monitoring/v3/agent_service.proto'),
+    group: googleProtoFiles('monitoring/v3/group_service.proto'),
+    metric: googleProtoFiles('monitoring/v3/metric_service.proto')
+  }
+}
+
 module.exports.pubsub = {
   v1: googleProtoFiles('pubsub/v1/pubsub.proto'),
   v1beta2: googleProtoFiles('pubsub/v1beta2/pubsub.proto')
@@ -44,4 +56,13 @@ module.exports.pubsub = {
 module.exports.speech = {
   v1: googleProtoFiles('cloud/speech/v1/cloud_speech.proto'),
   v1beta1: googleProtoFiles('cloud/speech/v1beta1/cloud_speech.proto')
+}
+
+
+module.exports.storagetransfer = {
+  v1: googleProtoFiles('storagetransfer/v1/transfer.proto')
+}
+
+module.exports.vision = {
+  v1: googleProtoFiles('cloud/vision/v1/image_annotator.proto')
 }
