29fac04d05673822388983a0a96dcca3da5fb0da
vijay-qlogic <36055624+vijay-qlogic@users.noreply.github.com>
2018-12-21 12:11:17 +0530

    refactor: removed async module dependency (#120)

--- tools/prepublish.ts
+++ tools/prepublish.ts
@@ -4,8 +4,7 @@
  * Distributed under MIT license.
  * See file LICENSE for detail or copy at https://opensource.org/licenses/MIT
  */
-
-import * as async from 'async';
+import {promisify} from '@google-cloud/promisify';
 import * as fs from 'fs';
 import * as got from 'got';
 import * as path from 'path';
@@ -37,44 +36,32 @@ const extract = (input, opts, callback) => {
       });
 };
 
-async.series(
-    [
-      (next) => {
-        require('child_process').exec('rm -rf google', next);
-      },
+const extractAsync = promisify(extract);
+const execAsync = promisify(require('child_process').exec);
 
-      (next) => {
-        extract(
-            'https://github.com/googleapis/googleapis/archive/master.zip', {
-              strip: 1,
-            },
-            next);
-      },
+async function main() {
+  await execAsync('rm -rf google');
 
-      (next) => {
-        extract(
-            'https://github.com/google/protobuf/archive/master.zip', {
-              strip: 2,
-              filter: (file) => {
-                return (
-                    file.parent.indexOf('protobuf-master') === 0 &&
-                    file.parent.indexOf('protobuf-master/src/') === 0 &&
-                    file.parent.indexOf('/compiler') === -1 &&
-                    file.parent.indexOf('/internal') === -1 &&
-                    file.filename.indexOf('unittest') === -1 &&
-                    file.filename.indexOf('test') === -1);
-              },
-            },
-            next);
-      },
+  await extractAsync(
+      'https://github.com/googleapis/googleapis/archive/master.zip', {
+        strip: 1,
+      });
 
-      (next) => {
-        require('child_process')
-            .exec(
-                '[ -d "overrides" ] && cp -R overrides/* google || echo "no overrides"',
-                next);
-      },
-    ],
-    (err) => {
-      if (err) throw err;
-    });
+  await extractAsync('https://github.com/google/protobuf/archive/master.zip', {
+    strip: 2,
+    filter: (file) => {
+      return (
+          file.parent.indexOf('protobuf-master') === 0 &&
+          file.parent.indexOf('protobuf-master/src/') === 0 &&
+          file.parent.indexOf('/compiler') === -1 &&
+          file.parent.indexOf('/internal') === -1 &&
+          file.filename.indexOf('unittest') === -1 &&
+          file.filename.indexOf('test') === -1);
+    },
+  });
+
+  await execAsync(
+      '[ -d "overrides" ] && cp -R overrides/* google || echo "no overrides"');
+}
+
+main().catch(console.error);
