a785c5e67b28c9d84ccc53c35a4f5038ddebd96c
yoshi-automation <yoshi-automation@google.com>
2020-04-01 05:21:08 -0700

    build!: update to latest gts and TypeScript (#271)
    https://github.com/googleapis/nodejs-proto-files/commit/81c24ecde3bc01b54fb454752288f962a1ea6312
    commit 81c24ecde3bc01b54fb454752288f962a1ea6312
    Author: Alex <7764119+AVaksman@users.noreply.github.com>
    Date:   Fri Mar 27 16:24:43 2020 -0400
    
        build!: update to latest gts and TypeScript (#271)

--- tools/prepublish.ts
+++ tools/prepublish.ts
@@ -1,15 +1,22 @@
-/**
- * Copyright 2018 Google LLC
- *
- * Distributed under MIT license.
- * See file LICENSE for detail or copy at https://opensource.org/licenses/MIT
- */
+// Copyright 2020 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 import {promisify} from 'util';
 import * as fs from 'fs';
 import * as got from 'got';
 import * as path from 'path';
 
-// tslint:disable-next-line variable-name
+// eslint-disable-next-line @typescript-eslint/no-var-requires
 const DecompressZip = require('decompress-zip');
 
 const extract = (input, opts, callback) => {
