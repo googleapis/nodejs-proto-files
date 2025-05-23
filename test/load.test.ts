d153635c62b8179799d819661b8b5e921527db84
Sofia Leon <sofialeon@google.com>
2025-05-23 16:02:42 -0700

    add module resolution

--- test/load.test.ts
+++ test/load.test.ts
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-import * as assert from 'assert';
+import assert from 'assert';
 import {describe, it} from 'mocha';
 import * as path from 'path';
 import * as protobuf from 'protobufjs';
